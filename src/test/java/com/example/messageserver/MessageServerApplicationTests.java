7e334834c73a17bb9292319204976309871966f7
yongjoon <penubokim@gmail.com>
2021-08-07 10:43:59 +0900

    spring initializr setup

---
+++ src/test/java/com/example/messageserver/MessageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
